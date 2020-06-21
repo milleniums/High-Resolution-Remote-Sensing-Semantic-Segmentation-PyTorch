05cc62fc99595b4e09d997ebf6571439c372de2e
milleniums <2926151134@qq.com>
2020-06-21 23:36:30 +0800

    add TensorboardX

--- train.py
+++ train.py
@@ -14,18 +14,19 @@ from torch.autograd import Variable
 import numpy as np
 from tqdm import tqdm
 from prettytable import PrettyTable
-from class_names import gid_classes
+import torchvision
 from torchvision import transforms
 from palette import colorize_mask
 from PIL import Image
 from collections import OrderedDict
+from tensorboardX import SummaryWriter
 
 def parse_args():
     parser = argparse.ArgumentParser(description="RemoteSensingSegmentation by PyTorch")
     # dataset
-    parser.add_argument('--dataset-name', type=str, default='rssrai2019_semantic_segmentation')
-    parser.add_argument('--train-data-root', type=str, default='../data_6_10/train')
-    parser.add_argument('--val-data-root', type=str, default='../data_6_10/val')
+    parser.add_argument('--dataset-name', type=str, default='five')
+    parser.add_argument('--train-data-root', type=str, default='../data_5_6_21/train')
+    parser.add_argument('--val-data-root', type=str, default='../data_5_6_21/val')
     parser.add_argument('--train-batch-size', type=int, default=32, metavar='N', help='batch size for training (default:16)')
     parser.add_argument('--val-batch-size', type=int, default=32, metavar='N', help='batch size for testing (default:16)')
     # output_save_path
@@ -35,7 +36,7 @@ def parse_args():
     parser.add_argument('--base-size', type=int, default=512, help='base image size')
     parser.add_argument('--crop-size', type=int, default=512, help='crop image size')
     parser.add_argument('--flip-ratio', type=float, default=0.5)
-    parser.add_argument('--resize-scale-range', type=str, default='1, 2.0')
+    parser.add_argument('--resize-scale-range', type=str, default='0.5, 2.0')
     # model
     parser.add_argument('--model', type=str, default='deeplabv3_version_1', help='model name')
     parser.add_argument('--backbone', type=str, default='resnet50', help='backbone name')
@@ -110,33 +111,37 @@ class Trainer(object):
         self.args = args
         resize_scale_range = [float(scale) for scale in args.resize_scale_range.split(',')]
         sync_transform = sync_transforms.Compose([
-            # sync_transforms.RandomScale(args.base_size, args.crop_size, resize_scale_range),
+            sync_transforms.RandomScale(args.base_size, args.crop_size, resize_scale_range),
             sync_transforms.RandomFlip(args.flip_ratio)
         ])
         self.resore_transform = transforms.Compose([
             DeNormalize([.485, .456, .406], [.229, .224, .225]),
             transforms.ToPILImage()
         ])
-
-        train_dataset = RSDataset(root=args.train_data_root, mode='train', sync_transforms=sync_transform)
-        self.train_loader = DataLoader(dataset=train_dataset,
+        self.visualize = transforms.Compose([transforms.ToTensor()])
+        
+        class_name = args.dataset_name
+        if class_name == 'fifteen': from class_names import fifteen_classes
+        if class_name == 'five': from class_names import five_classes
+        self.train_dataset = RSDataset(class_name, root=args.train_data_root, mode='train', sync_transforms=sync_transform)
+        self.train_loader = DataLoader(dataset=self.train_dataset,
                                        batch_size=args.train_batch_size,
                                        num_workers=args.num_workers,
                                        shuffle=True,
                                        drop_last=True)
-        print('class names {}.'.format(train_dataset.class_names))
-        print('Number samples {}.'.format(len(train_dataset)))
+        print('class names {}.'.format(self.train_dataset.class_names))
+        print('Number samples {}.'.format(len(self.train_dataset)))
         if not args.no_val:
-            val_data_set = RSDataset(root=args.val_data_root, mode='val', sync_transforms=sync_transform)
+            val_data_set = RSDataset(class_name, root=args.val_data_root, mode='val', sync_transforms=sync_transform)
             self.val_loader = DataLoader(dataset=val_data_set,
                                          batch_size=args.val_batch_size,
                                          num_workers=args.num_workers,
                                          shuffle=False,
                                          drop_last=True)
-        self.num_classes = len(train_dataset.class_names)
+        self.num_classes = len(self.train_dataset.class_names)
         print("类别数：", self.num_classes)
         self.class_loss_weight = torch.Tensor(args.class_loss_weight)
-        self.criterion = nn.CrossEntropyLoss(weight=self.class_loss_weight, reduction='mean', ignore_index=-1).cuda()
+        self.criterion = nn.CrossEntropyLoss(weight=None, reduction='mean', ignore_index=-1).cuda()
 
         n_blocks = args.n_blocks
         n_blocks = [int(b) for b in n_blocks.split(',')]
@@ -161,6 +166,9 @@ class Trainer(object):
                            atrous_rates=atrous_rates,
                            multi_grids=multi_grids,
                            output_stride=args.output_stride)
+        if args.model == 'hdc':
+            from models.HDC.duc_hdc import ResNetDUC
+            model = ResNetDUC(num_classes=self.num_classes)
         # print(model)
 
 
@@ -216,6 +224,7 @@ class Trainer(object):
             loss = self.criterion(outputs, masks)
 
             train_loss.update(loss, self.args.train_batch_size)
+            writer.add_scalar('train_loss', train_loss.avg, curr_iter)
             loss.backward()
             self.optimizer.step()
 
@@ -227,9 +236,12 @@ class Trainer(object):
                                                 label=masks.flatten(),
                                                 num_classes=self.num_classes)
         train_acc, train_acc_per_class, train_acc_cls, train_IoU, train_mean_IoU, train_kappa = metric.evaluate(conf_mat)
+        writer.add_scalar(tag='train_loss_per_epoch', scalar_value=train_loss.avg, global_step=epoch, walltime=None)
+        writer.add_scalar(tag='train_acc', scalar_value=train_acc, global_step=epoch, walltime=None)
+        writer.add_scalar(tag='train_kappa', scalar_value=train_kappa, global_step=epoch, walltime=None)
         table = PrettyTable(["序号", "名称", "acc", "IoU"])
         for i in range(self.num_classes):
-            table.add_row([i, gid_classes()[i], train_acc_per_class[i], train_IoU[i]])
+            table.add_row([i, self.train_dataset.class_names[i], train_acc_per_class[i], train_IoU[i]])
         print(table)
         print("train_acc:", train_acc)
         print("train_mean_IoU:", train_mean_IoU)
@@ -253,13 +265,18 @@ class Trainer(object):
             preds = preds.data.cpu().numpy().squeeze().astype(np.uint8)
             masks = masks.data.cpu().numpy().squeeze().astype(np.uint8)
             score = _.data.cpu().numpy()
+            val_visual = []
             for i in range(score.shape[0]):
-                num_score = np.sum(score[i] > 0.8)
-                if num_score > 0.9*(128*128):
+                num_score = np.sum(score[i] > 0.9)
+                if num_score > 0.9*(512*512):
                     img_pil = self.resore_transform(data[0][i])
                     preds_pil = Image.fromarray(preds[i].astype(np.uint8)).convert('L')
                     pred_vis_pil = colorize_mask(preds[i])
                     gt_vis_pil = colorize_mask(data[1][i].numpy())
+
+                    val_visual.extend([self.visualize(img_pil.convert('RGB')),
+                                       self.visualize(gt_vis_pil.convert('RGB')),
+                                       self.visualize(pred_vis_pil.convert('RGB'))])
 
                     dir_list = ['rgb', 'label', 'vis_label', 'gt']
                     rgb_save_path = os.path.join(self.save_pseudo_data_path, dir_list[0], str(epoch))
@@ -275,17 +292,37 @@ class Trainer(object):
                     preds_pil.save(os.path.join(path_list[1], 'label_%d_%d.png' % (index, i)))
                     pred_vis_pil.save(os.path.join(path_list[2], 'vis_%d_%d.png' % (index, i)))
                     gt_vis_pil.save(os.path.join(path_list[3], 'gt_%d_%d.png' % (index, i)))
+            if val_visual:
+                val_visual = torch.stack(val_visual, 0)
+                val_visual = torchvision.utils.make_grid(tensor=val_visual,
+                                                         nrow=3,
+                                                         padding=5,
+                                                         normalize=False,
+                                                         range=None,
+                                                         scale_each=False,
+                                                         pad_value=0)
+                writer.add_image(tag='pres&GTs', img_tensor=val_visual, global_step=None, walltime=None)
             conf_mat += metric.confusion_matrix(pred=preds.flatten(),
                                                 label=masks.flatten(),
                                                 num_classes=self.num_classes)
         val_acc, val_acc_per_class, val_acc_cls, val_IoU, val_mean_IoU, val_kappa = metric.evaluate(conf_mat)
+        writer.add_scalars(main_tag='val_single_acc',
+                           tag_scalar_dict={self.train_dataset.class_names[i]: val_acc_per_class[i] for i in range(len(self.train_dataset.class_names))},
+                           global_step=epoch, walltime=None)
+        writer.add_scalars(main_tag='val_single_iou',
+                           tag_scalar_dict={self.train_dataset.class_names[i]: val_IoU[i] for i in range(len(self.train_dataset.class_names))},
+                           global_step=epoch, walltime=None)
+        writer.add_scalar('val_acc', val_acc, epoch)
+        writer.add_scalar('val_acc_cls', val_acc_cls, epoch)
+        writer.add_scalar('val_mean_IoU', val_mean_IoU, epoch)
+        writer.add_scalar('val_kappa', val_kappa, epoch)
         model_name = 'epoch_%d_acc_%.5f_kappa_%.5f' % (epoch, val_acc, val_kappa)
         if val_kappa > self.args.best_kappa:
             torch.save(self.model.state_dict(), os.path.join(self.args.directory, model_name+'.pth'))
             self.args.best_kappa = val_kappa
         table = PrettyTable(["序号", "名称", "acc", "IoU"])
         for i in range(self.num_classes):
-            table.add_row([i, gid_classes()[i], val_acc_per_class[i], val_IoU[i]])
+            table.add_row([i, self.train_dataset.class_names[i], val_acc_per_class[i], val_IoU[i]])
         print(table)
         print("val_acc:", val_acc)
         print("val_mean_IoU:", val_mean_IoU)
@@ -294,6 +331,7 @@ class Trainer(object):
 
 if __name__ == "__main__":
     args = parse_args()
+    writer = SummaryWriter(args.directory)
     trainer = Trainer(args)
     if args.eval:
         # print("Evaluating model:", args.resume)
